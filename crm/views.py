ebb92817a5b9de2ade67f1b81ba0f49a617f7954
YOUR_NAME <YOUR_EMAIL>
2022-07-18 23:10:19 +0300

    Added base template, static folders

--- crm/views.py
+++ crm/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from .models import Order
+from .forms import OrderForm
 
 
 # Create your views here.
 def first_page(request):
     object_list = Order.objects.all()
+    form = OrderForm()
     return render(request, 'index.html', {
-        'object_list': object_list
+        'object_list': object_list,
+        'form': form
     })
 
 
 def thanks_page(request):
-    return render(request, 'thanks_page.html')
+    name = request.POST['name']
+    phone = request.POST['phone']
+    order = Order(order_name=name, order_phone=phone)
+    order.save()
+    return render(request, 'thanks_page.html', {
+        'name': name,
+        'phone': phone,
+        'req': request
+    })
