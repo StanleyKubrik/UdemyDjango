6a5a34e51d157f1386fc3aeab3b4054b898cfa2d
StanleyKubrik <37335329+StanleyKubrik@users.noreply.github.com>
2022-07-18 17:17:01 +0300

    Added thanks_page

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Order
+
 
 # Create your views here.
+def first_page(request):
+    object_list = Order.objects.all()
+    return render(request, 'index.html', {
+        'object_list': object_list
+    })
+
+
+def thanks_page(request):
+    return render(request, 'thanks_page.html')
