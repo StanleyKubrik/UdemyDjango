f7726d79003f099e7d1bf642935c419e5b2943ea
YOUR_NAME <YOUR_EMAIL>
2022-07-25 22:52:11 +0300

    Create class OrderCrm, added search in orders.

--- crm/admin.py
+++ crm/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Order, StatusCrm, CommentCrm
 
+
 # Register your models here.
-admin.site.register(Order)
+class OrderAdm(admin.ModelAdmin):
+    list_display = ('id', 'order_status', 'order_dt', 'order_name', 'order_phone')
+    list_display_links = ('id', 'order_name')
+    search_fields = ('id', 'order_dt', 'order_name', 'order_phone')
+
+
+admin.site.register(Order, OrderAdm)
 admin.site.register(StatusCrm)
 admin.site.register(CommentCrm)
