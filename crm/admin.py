7c51ba01fe07a6de7518774af31ee5a0656a3dc4
YOUR_NAME <YOUR_EMAIL>
2022-07-27 00:26:05 +0300

    Removed base_site.html to project from Lib. Improve admin panel.

--- crm/admin.py
+++ crm/admin.py
@@ -3,10 +3,24 @@ from .models import Order, StatusCrm, CommentCrm
 
 
 # Register your models here.
+class Comment(admin.StackedInline):
+    model = CommentCrm
+    fields = ('comment_dt', 'comment_text')
+    readonly_fields = ('comment_dt',)
+    extra = 0
+
+
 class OrderAdm(admin.ModelAdmin):
     list_display = ('id', 'order_status', 'order_dt', 'order_name', 'order_phone')
     list_display_links = ('id', 'order_name')
-    search_fields = ('id', 'order_dt', 'order_name', 'order_phone')
+    search_fields = ('id', 'order_status__status_name', 'order_dt', 'order_name', 'order_phone')
+    list_filter = ('order_status',)
+    list_editable = ('order_status',)
+    list_per_page = 10
+    list_max_show_all = 100
+    fields = ('id', 'order_dt', 'order_status', 'order_name', 'order_phone')
+    readonly_fields = ('id', 'order_dt')
+    inlines = [Comment]
 
 
 admin.site.register(Order, OrderAdm)
